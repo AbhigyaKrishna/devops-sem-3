3c32f45f5f8744d3a3aea8acba14c013bac31ae4
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-11-27 12:59:14 +0530

    feat: updated backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ ! -d "$2" ]; then
+    echo "Specified directory '$2' doesn't exist. Creating directory..."
+    mkdir -p "$2"
+fi
+
+time=$(date +"%Y-%m-%d_%H-%M-%S")
+
+if [ ! -d "$1" ]; then
+    echo "[${time}] Specified directory '$1' doesn't exist" | tee -a "$2/backup.log"
+    exit 1
+fi
+
+zip=false
 while getopts 'z' OPTION
 do
     case "${OPTION}" in
@@ -7,36 +20,24 @@ do
             zip=true
         ;;
         \?)
-            echo "$0: Error: Invalid option: -${OPTARG}" >&2
+            echo "[${time}] $0: Error: Invalid option: -${OPTARG}" | tee -a "$2/backup.log"
             exit 1
         ;;
     esac
 done
 
-if [[ ! -d "$1" ]]; then
-    echo "Specified directory '$1' doesn't exist"
-    exit 1
-fi
-
-if [[ ! -d "$2" ]]; then
-    echo "Specified directory '$2' doesn't exist"
-    exit 1
-fi
-
 shift $((OPTIND - 1))
 
-time=$(date +'%m-%d-%Y-%T')
-
-if [[ $zip ]]; then
-    tar -czvf "$2/backup-${time}.tar.gz" $1
+if [ $zip ]; then
+    tar -czvf "$2/backup-${time}.tar.gz" "$1"
     echo "Backed up at $2/backup-${time}.tar.gz"
 else
-    cp $1 "$2/backup-${time}"
+    cp "$1" "$2/backup-${time}"
     echo "Backed up at $2/backup-${time}"
 fi
 
-echo "[${time} UTC] Backup completed." >> "$2/backup.log"
+echo "[${time}] Backup completed." | tee -a "$2/backup.log"
 
 
 echo "Removing older backup files..."
-find "$2/*" -type f,d -mtime +6 -print | xargs rm -rf
+find "$2/*" -type f,d -mtime +6 -print0 | xargs rm -rf
